8be8325fd30b309f8481ffd10e9b0135ce65e8e0
vds06 <vds@vdsharma.org>
2017-02-10 03:46:18 +0000

    Add files via upload

--- Bitcoin_longshort_algorithm.py
+++ Bitcoin_longshort_algorithm.py
@@ -22,10 +22,24 @@ with open('bpx.csv') as csvfile:
     testelement = prices[2]
 
     len = len(prices) - 3
-#    print(len)
-#    print(testelement)
+
+with open('FXCSV2_4.csv') as csvfile:
+    reader2 = csv.reader(csvfile, delimiter=',')
+
+    rates = []
+
+    for row in reader2:
+        rate = row[0]
+        rates.append(rate)
+
+    ratetestelement = rates[2]
+
+#    fxlen = len(rates) - 3
 
 
+#    print(fxlen)
+    print(rates)
+    print(ratetestelement)
 
 def btcnow():
     btcnow = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
@@ -72,7 +86,73 @@ def btcpricingdatabase(len, date1txt, prices):
             ptstart = pt.find(date1txt) + 20
             ptend = pt.find(pendtxt, ptstart)
             pttext = pt[ptstart:ptend]
+
+
 #            print(pttext)
+            break
+    return (pttext)
+
+# FX pricing database function below operates in exactly the same way as the BTC pricing database function above
+
+def fxpricingdatabase(len, date1txt, curr): # len needs to be replaced with fxlen (to execute upto the length of the fx database)
+    for n in range(2, len):
+        nminusone = n - 1
+        ratetxt = str(rates[nminusone:n])
+        pendtxt = "'"
+        pt = ratetxt
+        date1txtslash = date1txt.replace("-","/")
+        print(date1txtslash)
+        result = pt.find(date1txtslash)
+        if result == -1:
+
+            pttext='0'
+            print ("shit we're fucked!")
+
+
+        elif result != -1:
+
+            string = pt
+            mylist = pt.split(',')
+
+            print(mylist)
+
+            inr = str(mylist[6:7])
+            cny = str(mylist[5:6])
+            gbp = str(mylist[4:5])
+
+            if curr is "INR" :
+                pttext = inr
+                pendtxt = "\\"
+                ptstart = pttext.find('"') + 1
+                ptend = pttext.find(pendtxt, ptstart)
+                pttext = pttext[ptstart:ptend]
+                print(pttext)
+
+
+
+            elif curr is "CNY" :
+                pttext = cny
+
+                ptstart = pttext.find("'") + 1
+                ptend = pttext.find(pendtxt, ptstart)
+                pttext = pttext[ptstart:ptend]
+                print(pttext)
+
+
+
+            elif curr is "GBP" :
+                pttext = gbp
+
+                ptstart = pttext.find("'") + 1
+                ptend = pttext.find(pendtxt, ptstart)
+                pttext = pttext[ptstart:ptend]
+                print(pttext)
+
+
+            print(pttext + "this is currency fetched")
+
+
+
             break
     return (pttext)
 
@@ -93,44 +173,60 @@ def volalgo(year, walletusd, vol, btcamt, walletbtc, currdate, currmonth, currye
 
     def meanvol(date1, date2, curr1, curr2):
 
-        url = 'http://api.fixer.io/'
-        base = '?base=' + curr1
+        databaseflag = 1
 
-        y1 = url + date1 + base
-        t1 = url + date2 + base
+        if databaseflag == 0 :
 
-        endtxt = ','
+            url = 'http://api.fixer.io/'
+            base = '?base=' + curr1
 
-        s = requests.get(y1)
-        s = s.text
-        sstart = s.find(curr2)+5
-        send = s.find(endtxt,sstart)
-        stext= s[sstart:send]
+            y1 = url + date1 + base
+            t1 = url + date2 + base
+
+            endtxt = ','
+
+            s = requests.get(y1)
+            s = s.text
+            sstart = s.find(curr2)+5
+            send = s.find(endtxt,sstart)
+            stext= s[sstart:send]
 
 
-        t = requests.get(t1)
-        t = t.text
-        tstart = t.find(curr2)+5
-        tend = t.find(endtxt,tstart)
-        ttext= t[tstart:tend]
+            t = requests.get(t1)
+            t = t.text
+            tstart = t.find(curr2)+5
+            tend = t.find(endtxt,tstart)
+            ttext= t[tstart:tend]
+
+        else:
+            stext = fxpricingdatabase(len,date1, curr2)
+            ttext = fxpricingdatabase(len,date2, curr2)
+
+            print(stext)
+            print(ttext)
 
         fx1= float(stext)
         fx2= float(ttext)
+
+        print(fx1)
+        print(fx2)
+
 
         fxdelta = fx2-fx1
         fxchangepct = (abs(fxdelta)/fx1)*100
         pct = str(fxchangepct)
 
+
         return(pct)
 
-    def usdpair(curr):
+    def usdpair(curr,date1txt1,date2txt1):
         mv = (meanvol(date1txt1,date2txt1,"USD",curr))
         return(mv)
 
     A = "GBP"
     #B = 'EUR'
-    C = 'CNY'
-    D = 'INR'
+    C = "CNY"
+    D = "INR"
 
     for y in range (1,10):
 
@@ -139,11 +235,11 @@ def volalgo(year, walletusd, vol, btcamt, walletbtc, currdate, currmonth, currye
             for month in range (0,11):
 
                 month = month + 1
-                date1 = 0
-                date2 = 1
+                date1 = 1
+                date2 = 2
                 for d in range (1,31):  #needs fixing only running upto the 28th of the month atm
 #                    if not(((date1 == currdate) & (month == currmonth) & (year == curryear))):
-                        date1 = date1 + 1
+                        date1 = date1
                         date2 = date1 + 1
 
                         d = date1
@@ -153,9 +249,16 @@ def volalgo(year, walletusd, vol, btcamt, walletbtc, currdate, currmonth, currye
                         elif ((date1 == currdate) & (month == currmonth) & (year == curryear)):
                             return(portfoliovalue)
                         else:
-                            date1txt = str(datetime.datetime(year, month, date1))
+
                             date2txt = str(datetime.datetime(year, month, date2))
 
+                        if (((date1 == 29) & (month == 2)) or (((date1 == 31) & ((month == 4) or (month == 6) or (month == 9) or (month == 11))))):
+                            break
+                        elif ((date1 == currdate) & (month == currmonth) & (year == curryear)):
+                            return (portfoliovalue)
+                        else:
+
+                            date1txt = str(datetime.datetime(year, month, date1))
 
                             # Truncation of date and time format
 
@@ -163,9 +266,9 @@ def volalgo(year, walletusd, vol, btcamt, walletbtc, currdate, currmonth, currye
 #                        print(date1txt1)
                         date2txt1 = date2txt[:10]
 
-                        fxdelta1 = float(usdpair(A))
-                        fxdelta2 = float(usdpair(C))
-                        fxdelta3 = float(usdpair(D))
+                        fxdelta1 = float(usdpair(A,date1txt1,date2txt1))
+                        fxdelta2 = float(usdpair(C,date1txt1,date2txt1))
+                        fxdelta3 = float(usdpair(D,date1txt1,date2txt1))
                         meanpct = mean(fxdelta1, fxdelta2, fxdelta3)
 
 #
